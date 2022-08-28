106bf5a2f1f8810e6249ee079bcab36cf9c5af92
Andrew Wang <Andrewruujin@gmail.com>
2022-08-28 19:44:37 -0400

    commit tree 1ee0fab2bb4fe7b9f4dea0bcf37a9621c880e9fa

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+function submitData(n, em){
+    const userInfo = {
+        name: n,
+        email: em
+    }
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(res => res.json())
+    .then(data => showID(data))
+    .catch(function (error) {
+        errorMessage(error.message)
+      })
+}
+
+function showID(data){
+    let h1 = document.createElement('h1')
+    h1.textContent = data.id
+    document.body.appendChild(h1)
+}
+
+function errorMessage(data){
+    let newDiv = document.createElement('div')
+    newDiv.innerHTML = `<h2>${data}</h2>`
+    document.body.appendChild(newDiv)
+    console.log(data)
+    /*
+    let h2 = document.createElement('h2')
+    h2.textContent = data
+    document.body.appendChild(h2)
+    console.log(h2.textContent)
+    console.log(data)*/
+}
+
+
+
